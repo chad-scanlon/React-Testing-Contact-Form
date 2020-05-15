27e07e3c3c0a3c4c2907bc5306ba063a0406364e
chad-scanlon <59572157+chad-scanlon@users.noreply.github.com>
2020-05-15 12:08:24 -0400

    Inputs and button tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,36 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
   render(<App />);
+});
+
+test("renders ContactForm without crashing", () => {
+  render(<ContactForm />);
+});
+
+test("name input field exists", () => {
+  const { getByTestId } = render(<ContactForm />);
+  getByTestId("nameInput");
+});
+
+test("last name input field exists", () => {
+  const { getByTestId } = render(<ContactForm />);
+  getByTestId("lastNameInput");
+});
+test("email input field exists", () => {
+  const { getByTestId } = render(<ContactForm />);
+  getByTestId("emailInput");
+});
+test("submit button works", async () => {
+  const firstName = "Tom";
+  const lastName = "Myspace";
+  const email = "tom@myspace.com";
+  const { getByTestId, getByText } = render(<ContactForm />);
+
+  const submit = getByTestId("submitButton");
+
+  fireEvent.click(submit);
 });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,6 +16,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
+            data-testid="nameInput"
             name="firstName"
             placeholder="Edd"
             ref={register({ required: true, maxLength: 3 })}
@@ -28,6 +29,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            data-testid="lastNameInput"
             name="lastName"
             placeholder="Burke"
             ref={register({ required: true })}
@@ -41,7 +43,11 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input
+            data-testid="emailInput"
+            name="email"
+            ref={register({ required: true })}
+          />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
@@ -55,7 +61,7 @@ const ContactForm = () => {
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input data-testid="submitButton" type="submit" />
       </form>
     </div>
   );
